ba36fc024af396501428e1816035d3bce65959e2
MychaelHaywood <mhaywood0512@tamu.edu>
2025-11-23 11:36:17 -0600

    Update #Data Dominators Walkability vs Obesity#.py

--- #Data Dominators Walkability vs Obesity#.py
+++ #Data Dominators Walkability vs Obesity#.py
@@ -119,3 +119,23 @@ df_counties.to_excel("df_tx_counties_health.xlsx", index=False)
 
 print("Cleaned file written to df_tx_counties_health.xlsx")
 
+#Mychael/Group Section# 
+import pandas as pd
+import matplotlib.pyplot as plt
+County_Health = pd.read_excel('df_tx_counties_health.xlsx', sheet_name='Sheet1')
+Walkability_Df = pd.read_excel('Walkability_County_Condensed.xlsx', sheet_name='Walkability_Condensed')
+Merged_df = County_Health.merge (Walkability_Df, how='left')
+print (Merged_df.head)
+Merged_df.to_excel('Merged_Data.xlsx', index=False)
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///my_database.db')
+Merged_df.to_sql('Merged_Main', con=engine, if_exists='replace', index=False)
+query="SELECT 'Texas County', 'TotalPopulation', 'TotalPop18plus', 'Food insecurity in the past 12 months among adults', 'No leisure-time physical activity among adults', 'Obesity among adults', 'pct_low_wage_emp',	'pct_med_wage_emp',	'pct_hi_wage_emp', 'pct_low_wage_wrk', 'pct_med_wage_wrk',	'pct_hi_wage_wrk',	'HH_total', '0_autos_pct',	'1_autos_pct',	'2_autos_pct',	'wtd_WrkAge_pop_pct',	'wtd_avg_walk_index' FROM Merged_Main;"
+Results_df = pd.read_sql(query,engine) 
+print (Results_df.head)
+print (Results_df.describe(include='all'))
+
+##----------- Analysis ------------## 
+
+
+
